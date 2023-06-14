4f7ef3eec44c73bcc8f693619e919bd98ddb4fd4
iamgunes <99250362+iamgunes@users.noreply.github.com>
2023-06-14 16:59:28 +0300

    Add files via upload

--- arbitrage_helper.py
+++ arbitrage_helper.py
@@ -1,8 +1,9 @@
 import aiohttp
 
 
-async def arbitrage(item_tier,item_name):
-    url = 'https://west.albion-online-data.com/api/v2/stats/Prices/' + str(item_tier).upper() + '_' + str(item_name).upper() + '.json'
+async def arbitrage(item_tier, item_name):
+    url = 'https://west.albion-online-data.com/api/v2/stats/Prices/' + \
+        str(item_tier).upper() + '_' + str(item_name).upper() + '.json'
     print(url)
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
@@ -11,9 +12,10 @@ async def arbitrage(item_tier,item_name):
             others_data = []
             results = []
             for item in json_data:
-                if 'city' in item and 'sell_price_min' in item and 'quality' in item:
+                if 'city' in item and 'sell_price_min' in item and 'sell_price_min_date' and 'quality' in item:
                     city = item['city']
                     price = item['sell_price_min']
+                    price_date = item['sell_price_min_date']
                     quality = item['quality']
 
                     if quality == 1:
@@ -29,19 +31,25 @@ async def arbitrage(item_tier,item_name):
 
                     if int(price) != 0:
                         if city == 'Black Market':
-                            bm_data.append((price, new_quality, city))
+                            bm_data.append(
+                                (price, new_quality, city, price_date))
                         if city != 'Black Market':
-                            others_data.append((price, new_quality, city))
-            for i in bm_data:  # Check profit
+                            others_data.append(
+                                (price, new_quality, city, price_date))
+            # Check profit
+            for i in bm_data:
                 for k in others_data:
                     if i[1] == k[1] and i[0] - k[0] > 0:
                         bm_price = i[0]
+                        bm_date = i[3]
                         others_price = k[0]
                         others_city = k[2]
+                        others_date = k[3]
                         profit = bm_price - others_price
                         qualities = k[1]
+
                         results.append(
-                            (bm_price, others_price, others_city, qualities, profit))
+                            (bm_price, others_price, others_city, qualities, profit, bm_date, others_date))
             if results:
                 return results
             else:
