e2c2237f2878454ffe4c4ddebecd0993d69826f3
iamgunes <99250362+iamgunes@users.noreply.github.com>
2023-07-12 12:04:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -41,6 +41,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    user = str(message.author.name)
+    user_message = str(message.content)
+    channel = str(message.channel)
+
+    print(user + ":" + " " + user_message +
+          " " + "at" + " " + channel.capitalize())
+
     if message.author == client.user:
         return
 
@@ -70,7 +77,8 @@ async def on_message(message):
             )
 
             embed = discord.Embed(
-                title=f"Price Of {item_name_finder(total)}"
+                title=f"Price Of {item_name_finder(total)}",
+                url='https://github.com/iamgunes'
             )
             embed.add_field(
                 name="Location", value=location, inline=True
@@ -84,10 +92,6 @@ async def on_message(message):
             embed.set_thumbnail(
                 url="https://render.albiononline.com/v1/item/" + item_id
             )
-            embed.set_footer(
-                text="Prices data from albion data. It is not provide live data."
-            )
-
             await message.channel.send(embed=embed)
         else:
             await message.channel.send("Item price not found")
@@ -96,6 +100,7 @@ async def on_message(message):
         split = user_message.split()
         if len(split) > 2 and len(split) < 5:
             item_tier = split[1]
+            item_tier_old = split[1]
             item_tier = item_tier.lower()
             item_tier = tiers.get(item_tier, item_tier)
             item_name = split[2]
@@ -118,15 +123,15 @@ async def on_message(message):
                     for _, _, _, _, profit, preProfit, _, _ in results
                 )
 
-                # If you want to check item uptade time you can delete the quotes
-                """blackMarketDate = "\n".join(
+                # If you want to check uptade date you can unlock
+                blackMarketDate = "\n".join(
                     f"{datetime.strptime(bm_date, '%Y-%m-%dT%H:%M:%S')}"
                     for _, _, _, _, _, _, bm_date, _ in results
-                )"""
-                """otherCitiesDate = "\n".join(
-                    f"{datetime.strptime(others_date, '%Y-%m-%dT%H:%M:%S')}"
-                    for _, _, _, _, _, _, _, others_date in results
-                )"""
+                )
+                otherCitiesDate = "\n".join(
+                    f"{others_city} - {datetime.strptime(others_date, '%Y-%m-%dT%H:%M:%S')}"
+                    for _, _, others_city, _, _, _, _, others_date in results
+                )
 
                 embed = discord.Embed(
                     title=f"Arbitrage Of {item_name_finder(total)}",
@@ -140,26 +145,53 @@ async def on_message(message):
                 embed.add_field(
                     name="Profit", value=profit, inline=True
                 )
+                try:
+                    embed.add_field(
+                        name="Black Market Amount", value=open(str(item_tier_old) + str(item_name),"r").read(), inline=False
+                    )
+                except:
+                    embed.add_field(
+                        name="Black Market Amount", value="Not defined", inline=False
+                    )
                 embed.set_thumbnail(
                     url="https://render.albiononline.com/v1/item/" + item_id
                 )
-                embed.set_footer(
-                    text="Prices data from albion data. It is not provide live data."
-                )
 
-                # If you want to check item uptade time you can delete the quotes
+
+                # If you want to check uptade date you can unlock
                 """embed.add_field(
                     name="Black Market Last Uptade", value=blackMarketDate, inline=True
                 )"""
                 """embed.add_field(
                     name="Other Cities Last Uptade", value=otherCitiesDate, inline=True
                 )"""
+                embed.set_footer(
+                    text="This results are from albion online data. It is not provide live data"
+                )
 
                 await message.channel.send(embed=embed)
             else:
                 await message.channel.send("Item is not profitable")
         else:
             await message.author.send("This is a wrong usage you can use like this:\n!arbitrage t4 leather")
+
+    if message.content.startswith("!add"):
+        split = user_message.split()
+        item_tier = split[1]
+        item_name = split[2]
+        item_quality = split[3]
+        item_amount = split[4]
+
+    if message.content.startswith("!add"):
+        split = user_message.split()
+        item_tier = split[1]
+        item_name = split[2]
+        item_quality = split[3]
+        item_amount = split[4]
+
+        with open(str(item_tier) + str(item_name),"a") as appendfile:
+            appendfile.write(item_tier + " " + item_name + " " + item_quality + " " + item_amount + "\n")
+            appendfile.close()
 
 
 client.run(discord_token)
